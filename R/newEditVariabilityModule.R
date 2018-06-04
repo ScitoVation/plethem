d98e1d66e907ac3b5c750c9a71ed734c47cfb774
spendse <spendse@scitovation.com>
2018-06-04 16:12:52 -0400

    MC now populates UI correctly for new paramter sets

--- R/newEditVariabilityModule.R
+++ R/newEditVariabilityModule.R
@@ -6,6 +6,7 @@ newEditVariabilityUI <- function(namespace){
   ns <- NS(namespace)
   showModal(modalDialog(
     title = "Variablility",
+    size = "l",
     tagList(
       fluidRow(
         column(8,
@@ -22,7 +23,18 @@ newEditVariabilityUI <- function(namespace){
                )
       ),
       textOutput(ns("debug_text")),
-      tags$div(id = ns("cv_ui"))
+      tags$div(id = ns("cv_ui"),
+               fluidRow(
+                 column(4,
+                        tags$h3("Parameter Name")
+                        ),
+                 column(3,
+                        tags$h3("Coefficient of Variation")
+                        ),
+                 column(3,
+                        tags$h3("Type of Distribution")
+                        )
+               ))
     ),
     footer = tagList(
       actionButton(ns("ok"),"Done"),
@@ -34,31 +46,52 @@ newEditVariabilityUI <- function(namespace){
 newEditVariability <- function(input,output,session,set_type,ops_type,var_params_list){
   
   ns <- session$ns
+  param_names <- names(var_params_list)
   div_id <-paste0("#",ns(""),"cv_ui") 
-  current_list <- list()
+  data4module  <- list()
+  data4module$current_list <- list()
   updatePickerInput(session,"param_names",choices = var_params_list)
   output$debug_text <- renderText({input$param_names})
   observeEvent(input$update,{
-    add_list <- setdiff(input$param_names,current_list())
+    current_list <- data4module$current_list
+    print(current_list)
+    add_list <- setdiff(input$param_names,current_list)
     print(add_list)
-    remove_list <- setdiff(current_list(),input$param_names)
+    remove_list <- setdiff(current_list,input$param_names)
     print(remove_list)
     # if new elements are selected, add UI
     if(length(add_list) != 0){
       for(x in add_list){
         insertUI(selector = div_id,
-                 ui = tagList(fluidRow(
-                   column(4,
-                          tags$h5(x)),
-                   column(3,
-                          numericInput(ns(paste0("cv_",x)),label = "CV",value = 0))
-                   ))
+                 ui = tagList(
+                   tags$div(id = ns(paste0("div_",x)),
+                            fluidRow(
+                              column(4,
+                                     param_names[which(var_params_list == x)]
+                                     ),
+                              column(3,
+                                     numericInput(ns(paste0("cv_",x)),label = NULL,value = 0)
+                                     ),
+                              column(3,
+                                     selectInput(ns(paste0("type_",x)),label = NULL,
+                                                 choices = c("Normal"="norm",
+                                                             "Log-normal"="lnorm"))
+                                     )
+                   )
+                   )
+                 )
                  )
       }
-    }else if(length(remove_list)!= 0){
+    }
+    if(length(remove_list)!= 0){
+      for(x in remove_list){
+        removeUI(session = session,
+                 selector = paste0("#",ns(paste0("div_",x))),
+                 immediate = T)
+      }
       
     }
-    current_list <<- reactiveVal(input$param_list)
+    data4module$current_list <<- input$param_names
 
   })
   
