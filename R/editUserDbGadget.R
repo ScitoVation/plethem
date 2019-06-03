453e419ad62898f5a48276e6eb0d385b6cfdf57c
spendse <salil.pendse@gmail.com>
2019-06-03 08:52:09 -0400

    Added project to git ignore and continue working on create user db gadgdet

--- R/editUserDbGadget.R
+++ R/editUserDbGadget.R
@@ -9,16 +9,26 @@ editUserDb <- function(file_path){
     gadgetTitleBar(title = "User Database",left = miniTitleBarCancelButton("cancel"),
                  right = miniTitleBarButton("ok","Save changes")),
     miniTabstripPanel(id = "tabpanel",
+                      between = tagList(
+                                pickerInput("model_type","Select model type",
+                                    choices = list("rapidPBPK"="pbpk",
+                                                   "Trout"="trout"),
+                                    inline = T,multiple = F,
+                                    width = validateCssUnit("100%"))
+                      ),
                       miniTabPanel("Chemical",
                                    miniContentPanel(
                                      fillPage(
-                                       fillCol(flex = c(1,1),
-                                         fillRow(flex = c(1,1),
-                                                 pickerInput(inputId = "chem_datasets",
-                                                            label = "Select Datasets to remove",
-                                                            choices = c("a","b","c")
-                                                            ),
-                                                 fileInput("chemfile_upload","Select Chemical File")
+                                       fillCol(flex = c(1,4),
+                                         fillRow(flex = c(6,2),
+                                                 # pickerInput(inputId = "chem_datasets",
+                                                 #            label = "Select Datasets to remove",
+                                                 #            choices = c("a","b","c")
+                                                 #            ),
+                                                 fileInput("chemfile_upload","Select Chemical File"),
+                                                 awesomeRadio("chem_select",inline = T,
+                                                              "Select All Rows",choices = c("Yes","No"),
+                                                              width = validateCssUnit("100%"))
                                          ),
                                          fillRow(
                                            DT::DTOutput("import_chem_data")
@@ -39,6 +49,9 @@ editUserDb <- function(file_path){
       stopApp(returnValue = "No changes saved")
     })
     observeEvent(input$ok,{
+      chem_rows <- input$import_chem_data_rows_selected
+      chem_save_data <- chem_data()[chem_rows,]
+      print(chem_save_data)
       stopApp(returnValue = "Database updated")
     })
     # The selected chemical file
@@ -48,10 +61,13 @@ editUserDb <- function(file_path){
     
     # The user's data, parsed into a data frame
     chem_data <- reactive({
+      validate(need(!(is.null(input$chemfile_upload)),
+                    message = "No file selected"))
+      
+      #req(input$chem_file_upload)
       if(!(is.null(input$chemfile_upload))){
         ret_dat <- read.csv(chemFile()$datapath)
-      }else{
-        ret_dat <- data.frame()
+        
       }
       
       
@@ -59,12 +75,18 @@ editUserDb <- function(file_path){
       return(ret_dat)
       
     })
-    output$import_chem_data <- DT::renderDT(DT::datatable(chem_data(),
-                                               caption = "Chemical Sets to import",
+    chem_view_table <- reactive({DT::datatable(chem_data(),
+                                               caption = NULL,
                                                rowname = NULL,
-                                               options= list(dom = "tp",pageLength = 5)),
-                                 server = T
-    )
+                                               options= list(dom = "tp",
+                                                             pageLength = 5,
+                                                             buttons = c("selectAll"))) %>% DT::formatSignif(c(5,6,7),4)
+                                               
+      })
+  
+    output$import_chem_data <- DT::renderDT(chem_view_table(),
+                                            server = T)
+    
   }
-  runGadget(ui,server,viewer =dialogViewer("IVIVE",width = 1200,height = 1800))
+  runGadget(ui,server,viewer =dialogViewer("Create user database",width = 1800,height = 1800))
 }
