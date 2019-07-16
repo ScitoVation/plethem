c675e0533d811548b134554c4d1bb8590aff6054
kbronson <kbronson@scitovation.com>
2019-07-16 12:42:53 -0400

    Added Reverse Dosimetry's ui.R, server.R and www folder.

---
+++ inst/reverseDosimetry/server.R
@@ -0,0 +1,538 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(RSQLite)
+library(DT)
+library(shinyWidgets)
+library(magrittr)
+library(dplyr)
+library(plotly)
+library(shinyjs)
+library(shinyBS)
+library(ggplot2)
+library(readr)
+library(tidyr)
+library(htmltools)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output,session) {
+  mcvals <- reactiveValues(
+    csvFile = NULL,
+    name = NULL
+  )
+  bmvals <- reactiveValues(
+    csvFile = NULL,
+    name = NULL
+  )
+  shinyjs::useShinyjs()
+  dataset <- reactiveValues()
+  dataset$savedat <- reactiveVal(c("No","none"))
+  
+  monteCarloModal <- function() {
+    modalDialog(
+      useShinyjs(),
+      title = "Upload Monte Carlo Results",
+      easyClose = TRUE,
+      size = "l",
+      tagList(
+        textInput(
+          "mcname",
+          "Dataset Name",
+          placeholder = "Enter name for the dataset"
+        ),
+        fluidRow(
+          column(
+            6,
+            shinyWidgets::radioGroupButtons(
+              "type",
+              "Select Exposure Type",
+              choices = c("Inhalation", "Oral", "IV", "Dermal")
+            )
+          ),
+          column(
+            6,
+            uiOutput(
+              "unit_ui"
+            )
+          )
+        ),
+        fileInput(
+          "csvFile",
+          label = "Select CSV file",
+          accept = c("text/csv","text/comma-separated-values",".csv"),
+          multiple = TRUE
+        ),
+        tags$h4("Results in mg/L")
+      ),
+      footer= tagList(
+        shinyjs::disabled(actionButton("add","Add Dataset")),
+        modalButton("Cancel")
+      )
+    )
+  }
+  
+  biomonitoringModal <- function() {
+    modalDialog(
+      useShinyjs(),
+      title = "Upload Biomonitoring Results",
+      easyClose = TRUE,
+      size = "l",
+      tagList(
+        textInput(
+          "bmname",
+          "Dataset Name",
+          placeholder = "Enter name for the dataset"
+        ),
+        shinyWidgets::radioGroupButtons(
+          "bmtype",
+          "Select Type",
+          choices = c("Parent", "Metabolite")
+        ),
+        fileInput(
+          "bmFile",
+          label = "Upload Biomonitoring Results",
+          accept = c("text/csv","text/comma-separated-values",".csv"),
+          multiple = TRUE
+        ),
+        tags$h4(tags$span(style='color:red', 'IMPORTANT:'), ' Biomonitoring results must be in mg/L', sep = '')
+      ),
+      footer= tagList(
+        shinyjs::disabled(actionButton("addBM","Add Dataset")),
+        modalButton("Cancel")
+      )
+    )
+  }
+  
+  observeEvent(input$csvFile, {
+    shinyjs::enable("add")
+  })
+  
+  observeEvent(input$bmFile, {
+    shinyjs::enable("addBM")
+  })
+  
+  observeEvent(input$btnUploadMC,{
+    showModal(monteCarloModal())
+  })
+  
+  observeEvent(input$btnUploadBMResults, {
+    showModal(biomonitoringModal())
+  })
+  
+  m  = list(
+    # l = 10,
+    # r = 10,
+    b = 80,
+    t = 80
+    # ,pad = 10
+  )
+  
+  # Actions on Add button in Upload Monte Carlo's Modal
+  observeEvent(input$add, {
+    mcvals$name <- input$mcname
+    filesIn <- input$csvFile
+    df_list <- lapply(filesIn$datapath,read_csv) # read each file into a data frame
+    mcvals$csvFile <- bind_rows(df_list) # concatenates all of the data frames
+    updatemenus <- list(
+      list(
+        active = 0,
+        type = 'buttons',
+        buttons = list(
+          list(
+            label = 'Default',
+            method = 'relayout',
+            args = list(
+              list(
+                yaxis = list(
+                  title = paste('Concentrations (mg/L)', sep = ''),
+                  type = 'linear'
+                )
+              )
+            )
+          ),
+          list(
+            label = 'Log Y-Axis',
+            method = 'relayout',
+            args = list(
+              list(
+                yaxis = list(
+                  title = paste('Log Concentrations (mg/L)', sep = ''),
+                  type = 'log'
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+    
+    output$Plot1 <- renderPlotly({
+      p <- plot_ly(
+        stack(mcvals$csvFile),
+        x = ~ind,
+        y = ~values,
+        type = "box"
+      ) %>%
+        layout(
+          title = mcvals$name,
+          yaxis = list(
+            title = paste('Concentrations (mg/L)', sep = '')
+          ),
+          xaxis = list(
+            title = paste(input$type, ' Exposure (',input$unit,')', sep = '')
+          ),
+          margin = m,
+          updatemenus = updatemenus
+        )
+    })
+    
+    removeModal()
+  })
+  
+  # Actions on Add button in Upload Biomonitoring's Modal
+  observeEvent(input$addBM, {
+    bmvals$name <- input$bmname
+    filesIn2 <- input$bmFile
+    df_list2 <- lapply(filesIn2$datapath,read_csv) # read each file into a data frame
+    bmvals$csvFile <- bind_rows(df_list2) # concatenates all of the data frames
+    bmCSV <- data.frame(bmvals$csvFile)
+    updatemenus <- list(
+      list(
+        active = 0,
+        type = 'buttons',
+        buttons = list(
+          list(
+            label = 'Default',
+            method = 'relayout',
+            args = list(
+              list(
+                yaxis = list(
+                  title = 'Count',
+                  type = 'linear'
+                )
+              )
+            )
+          ),
+          list(
+            label = 'Log Y-Axis',
+            method = 'relayout',
+            args = list(
+              list(
+                yaxis = list(
+                  title = 'Count',
+                  type = 'log'
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+    
+    output$Plot3 <- renderPlotly({
+      p <- plot_ly(
+        x = bmCSV[,1],
+        type = "histogram"
+      ) %>%
+        layout(
+          title = bmvals$name,
+          yaxis = list(
+            title = 'Count'
+          ),
+          xaxis = list(
+            title = paste(input$bmtype, ' Concentrations (mg/L)', sep = '')
+          ),
+          margin = m,
+          updatemenus = updatemenus
+        )
+    })
+    
+    removeModal()
+  })
+  
+  observeEvent(input$type,{
+    if(input$type == "Inhalation"){
+      choices = c("ppm","ppb")
+    }else if(input$type == "Oral"){
+      choices = c("mg/kg/day", "mg/L")
+    }else if(input$type == "IV"){
+      choices = c("mg/h")
+    }else if(input$type == "Dermal"){
+      choices = c("mg/cm2")
+    }else{
+      choices = c("unknown exposure type")
+    }
+    output$unit_ui <- renderUI({
+      shinyWidgets::radioGroupButtons(
+        "unit",
+        label = "Select Exposure Units",
+        choices = choices
+      )
+    })
+  })
+  
+  output$toggleSidebar <- reactive({
+    input$showpanel
+  })
+  
+  outputOptions(output, "toggleSidebar", suspendWhenHidden = F)
+  
+  observeEvent(input$btnRunRevDos,{
+    ppbFiles <- list.files(pattern = 'PercentilePPB.*csv') # list of all of the files to import
+    df_list <- lapply(ppbFiles,read_csv) # read each file into a data frame
+    percentileDF <- as.data.frame(bind_rows(df_list)) # concatenates all of the data frames
+    
+    plotFiles <- list.files(pattern = 'PDFandCDF.*csv') # list of all of the files to import
+    plotdf_list <- lapply(plotFiles,read_csv) # read each file into a data frame
+    pdfAndCDF <- as.data.frame(bind_rows(plotdf_list)) # concatenates all of the data frames
+    
+    updatemenus <- list(
+      list(
+        active = 0,
+        type = 'buttons',
+        buttons = list(
+          list(
+            label = 'Log X-Axis',
+            method = 'relayout',
+            args = list(
+              list(
+                xaxis = list(
+                  title = colnames(pdfAndCDF)[1],
+                  type = 'log'
+                )
+              )
+            )
+          ),
+          list(
+            label = 'Linear X-Axis',
+            method = 'relayout',
+            args = list(
+              list(
+                xaxis = list(
+                  title = colnames(pdfAndCDF)[1],
+                  type = 'linear'
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+    
+    output$percentilePPB <- DT::renderDataTable({
+      DT::datatable(
+        data = percentileDF,
+        extensions = 'Buttons',
+        class = 'cell-border stripe',
+        rownames = F,
+        options = list(
+          ordering = F,
+          autoWidth = T,
+          dom='rtB',
+          buttons = list(
+            list(
+              extend = 'csv',
+              text = 'Download CSV',
+              filename = "Percentile.csv"
+            )
+          ),
+          columnDefs = list(
+            list(
+              className = 'dt-center',
+              targets = c(0,1)
+            )
+          )
+        )
+      )
+    })
+    
+    output$PDF <- renderPlotly({
+      p <- plot_ly(
+        pdfAndCDF,
+        x = pdfAndCDF[[1]],
+        y = pdfAndCDF[[2]],
+        name = 'PDF',
+        type = 'scatter',
+        mode = 'lines+markers'
+      ) %>%
+        layout(
+          title = 'PDF',
+          xaxis = list(
+            title = colnames(pdfAndCDF)[1],
+            type = 'log'
+          ),
+          yaxis = list(
+            title = 'Probability'
+          ),
+          margin = m,
+          updatemenus = updatemenus
+        )
+    })
+    
+    output$CDF <- renderPlotly({
+      p <- plot_ly(
+        pdfAndCDF,
+        x = pdfAndCDF[[1]],
+        y = pdfAndCDF[[3]],
+        name = 'CDF',
+        type = 'scatter',
+        mode = 'lines+markers'
+      ) %>%
+        layout(
+          title = 'CDF',
+          xaxis = list(
+            title = colnames(pdfAndCDF)[1],
+            type = 'log'
+          ),
+          yaxis = list(
+            title = 'Cumulative'
+          ),
+          margin = m,
+          updatemenus = updatemenus
+        )
+    })
+    
+    revDosDataHeaders <- read.csv(
+      'ReverseDosimetryDataHeaders.csv',
+      header=FALSE
+    )
+    revDosData1 <- read.csv(
+      'ReverseDosimetryData1.csv',
+      header=FALSE
+    )
+    revDosData2 <- read.csv(
+      'ReverseDosimetryData2.csv',
+      header=FALSE
+    )
+    revDosData3 <- read.csv(
+      'ReverseDosimetryData3.csv',
+      header=FALSE
+    )
+    row.names(revDosData1) <- pdfAndCDF[[1]]
+    row.names(revDosData2) <- list('Adjusted Weighting Factors')
+    row.names(revDosData3) <- pdfAndCDF[[1]]
+    
+
+    sketch = htmltools::withTags(
+      table(
+        class = 'display',
+        thead(
+          style='text-align: right;',
+          tr(
+            th(colspan = 1, 'Larger than', style='text-align: left; background-color: #f5f5f5'),
+            lapply(rep(revDosDataHeaders[[1]], 1), th)
+          ),
+          tr(
+            th(colspan = 1, 'Smaller than or equal to', style='text-align: left; background-color: #f5f5f5'),
+            lapply(rep(revDosDataHeaders[[2]], 1), th)
+          )
+        )
+      )
+    )
+    
+    revDosDataTable <- function(userData){
+      DT::datatable(
+        data = userData,
+        container = sketch,
+        rownames = T,
+        extensions = c('FixedColumns','FixedHeader'),
+        options = list(
+          ordering = F,
+          autoWidth = T,
+          fixedColumns = T,
+          scrollX = '100%',
+          scrollY = 600,
+          scrollCollapse = T, # When scrollY is defined, bottom of table won't "float" below the table
+          pageLength = 50, # How many rows to display by default
+          # ,dom='rt',
+          columnDefs = list(
+            list(
+              width = '160px',
+              targets = c(0)
+            )
+          )
+        )
+      )
+    }
+    
+    output$revDosData1 <- DT::renderDataTable({
+      revDosDataTable(revDosData1)
+    })
+    
+    output$revDosData2 <- DT::renderDataTable({
+      revDosDataTable(revDosData2)
+    })
+    
+    output$revDosData3 <- DT::renderDataTable({
+      revDosDataTable(revDosData3)
+    })
+    
+    # Creates the dataframe for the user to download
+    td1 <- data.frame(t(revDosDataHeaders))
+    colnames(td1) = colnames(revDosData1)
+    dlTable1 <- bind_rows(td1, revDosData1)
+    rownames(dlTable1) = c('Larger than','Smaller than or equal to',pdfAndCDF[[1]])
+    
+    output$downloadTablerevDosData1 <- downloadHandler(
+      filename = "Probability_Table.csv",
+      function(file){
+        write.table(
+          dlTable1,
+          file = file,
+          sep = ",",
+          row.names = T,
+          col.names = F
+        )
+      }
+    )
+    
+    # Creates the dataframe for the user to download
+    dlTable2 <- bind_rows(td1, revDosData2)
+    rownames(dlTable2) = c('Larger than','Smaller than or equal to','Adjusted Weighting Factors')
+    
+    output$downloadTablerevDosData2 <- downloadHandler(
+      filename = "Adjusted_Weighting_Factors.csv",
+      function(file){
+        write.table(
+          dlTable2,
+          file = file,
+          sep = ",",
+          row.names = T,
+          col.names = F
+        )
+      }
+    )
+    
+    # Creates the dataframe for the user to download
+    dlTable3 <- bind_rows(td1, revDosData3)
+    rownames(dlTable3) = c('Larger than','Smaller than or equal to',pdfAndCDF[[1]])
+    
+    output$downloadTablerevDosData3 <- downloadHandler(
+      filename = "Weight_ECF.csv",
+      function(file){
+        write.table(
+          dlTable3,
+          file = file,
+          sep = ",",
+          row.names = T,
+          col.names = F
+        )
+      }
+    )
+    
+  })
+  
+  observeEvent(input$navbar,{
+    if (input$navbar == "Quit"){
+      stopApp()
+    }
+  })
+  
+})
---
+++ inst/reverseDosimetry/ui.R
@@ -0,0 +1,229 @@
+library(shiny)
+library(RSQLite)
+library(DT)
+library(shinyWidgets)
+library(shinyBS)
+library(plotly)
+library(shinyjs)
+
+jscode <- "
+shinyjs.disableTab = function(name) {
+  var tab = $('#'+name);
+
+  tab.bind('click.tab', function(e) {
+    e.preventDefault();
+    return false;
+  });
+  tab.addClass('disabled-comp');
+}
+
+shinyjs.enableTab = function(name) {
+  var tab = $('#'+name);
+  tab.unbind('click.tab');
+  tab.removeClass('disabled-comp');
+}
+"
+
+shinyUI(
+  tagList(
+    tags$head(
+      tags$link(
+        rel='icon',
+        href="https://www.scitovation.com/wp-content/uploads/2019/02/cropped-ScitoVation_icon-32x32.png",
+        sizes="32x32"
+      )
+    ),
+    useShinyjs(),
+    includeCSS("www/styles.css"),
+    navbarPage(
+      title = tags$img(
+        height = 25,
+        src = "https://www.scitovation.com/wp-content/themes/scitovation/assets/images/logo/ScitoVation-Pioneers_in_Chemical_Safety_Assessment.svg"
+      ),
+      id = 'navbar',
+      position = 'fixed-top',
+      windowTitle = 'PLETHEM: Reverse Dosimetry',
+      fluid = T,
+      collapsible = T,
+      tabPanel(
+        title = 'Inputs',
+        icon = icon('line-chart'),
+        tags$style(
+          type='text/css',
+          'body {padding-top: 50px; #background-color: #db6e00;}'
+        ),
+        div(
+          Id = 'mySidenav',
+          class = 'sidenav',
+          style = 'width: 300px; padding-top: 80px;',
+          align = 'center',
+          bsButton(
+            'btnUploadMC',
+            'Upload Monte Carlo Results',
+            block = TRUE,
+            style = 'primary',
+            width = '80%'
+          ),
+          br(),
+          bsButton(
+            'btnUploadBMResults',
+            'Upload Biomonitoring Results',
+            block = TRUE,
+            style = 'primary',
+            width = '80%'
+          ),
+          br(),
+          bsButton(
+            'btnRunRevDos',
+            'Run Reverse Dosimetry',
+            block = TRUE,
+            style = 'primary',
+            width = '80%'
+          )
+        ),
+        div(
+          Id = 'main',
+          style = 'margin-left:300px; padding: 10px;',
+          span(
+            style='font-size:30px;cursor:pointer',
+            onclick='toggleNav()',
+            HTML('&#9776;')
+          ),
+          fluidRow(
+            column(
+              12,
+              wellPanel(
+                align='center',
+                tags$h3('Monte Carlo Results'),
+                plotlyOutput("Plot1", height = "600px")
+              ),
+              wellPanel(
+                align='center',
+                tags$h3('Biomonitoring Results'),
+                plotlyOutput("Plot3", height = 600)
+              )
+            )
+          )
+        ),
+        tags$script(
+          'function toggleNav() {
+          if(document.getElementById("mySidenav").style.width !== "0px"){
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft= "0";
+          } else{
+            document.getElementById("mySidenav").style.width = "300px";
+            document.getElementById("main").style.marginLeft = "300px";
+          }
+
+          // Triggers a resize event
+          var evt = document.createEvent("UIEvents");
+          evt.initUIEvent("resize", true, false, window, 0);
+          window.dispatchEvent(evt);
+          }
+        '
+        )
+      ),
+      tabPanel(
+        title = 'Output',
+        style = 'padding-top: 5px;',
+        icon = icon('table'),
+        tabsetPanel(
+          id = 'Modeloutput',
+          type = 'tabs',
+          tabPanel(
+            'Results',
+            fluidPage(
+              fluidRow(
+                column(
+                  8,
+                  offset = 2,
+                  align = 'center',
+                  DT::dataTableOutput('percentilePPB',width = 400)
+                )
+              )
+            )
+          ),
+          tabPanel(
+            'Plots',
+            fluidPage(
+              fluidRow(
+                column(
+                  12,
+                  br(),
+                  wellPanel(
+                    align='center',
+                    tags$h3('Probability Density Function'),
+                    plotlyOutput("PDF", height = "600px")
+                  ),
+                  wellPanel(
+                    align='center',
+                    tags$h3('Cumulative Density Function'),
+                    plotlyOutput("CDF", height = "600px")
+                  )
+                )
+              )
+            )
+          ),
+          tabPanel(
+            'Data',
+            fluidPage(
+              fluidRow(
+                column(
+                  12,
+                  br(),
+                  wellPanel(
+                    tags$style('.datatables th {text-align: right;} h3 {text-align: center;}'),
+                    tags$h3('Probability Table'),
+                    DT::dataTableOutput('revDosData1'),
+                    downloadButton('downloadTablerevDosData1',label = "Download Table")
+                  ),
+                  wellPanel(
+                    tags$h3('Adjusted Weighting Factors from Worksheet "Measured blood conc"'),
+                    DT::dataTableOutput('revDosData2'),
+                    downloadButton('downloadTablerevDosData2',label = "Download Table")
+                  ),
+                  wellPanel(
+                    tags$h3('Weight the ECFs with the Adjusted Weighting Factors'),
+                    DT::dataTableOutput('revDosData3'),
+                    downloadButton('downloadTablerevDosData3',label = "Download Table")
+                  )
+                )
+              )
+            )
+          )
+        )
+      ),
+      tabPanel(
+        title = 'About',
+        style = 'padding-top: 5px;',
+        icon = icon('info-circle'),
+        fluidPage(
+          fluidRow(
+            column(
+              12,
+              align = 'Center',
+              # Application title
+              h2('PLETHEM: Reverse Dosimetry')
+            )
+          ),
+          fluidRow(
+            column(
+              8,
+              offset = 2,
+              
+              h4(
+                p('PLETHEM: Reverse Dosimetry is a user-interface module used
+                to perform reverse dosimetry calculations for estimating exposure concentrations. 
+                The module requires the user to input two files containing the predicted biomarker 
+                concentrations from a Monte Carlo simulation and the biomonitoring results.'
+                )
+              )
+            )
+          )
+        )
+      ),
+      tabPanel(title = "Quit",icon = icon("power-off"))
+    )
+  )
+)
+
