7ea08d23189bf527f745528ac7b206aafee1c780
spendse <spendse@scitovation.com>
2019-11-26 14:53:01 -0500

    update NCA now works

--- R/NCAfunctions.R
+++ R/NCAfunctions.R
@@ -5,6 +5,7 @@
 #' @description Calculates  the The user will never need to call this function
 #' @param result Result from the forward dosimetry or individual montecarlo run
 #' @param mode Forward Dosimetry or Montecarlo
+#' @importFrom NonCompart sNCA IntAUC AUC
 #' @export
 #' 
 performPlethemNCA <- function(result,var_names,mode = "FD"){
@@ -15,7 +16,7 @@ performPlethemNCA <- function(result,var_names,mode = "FD"){
   conc_data <- result[var_names]
   nca_conc_list<- lapply(var_names,function(x,conc_data,time){
     conc <- conc_data[[x]]
-    NCA_res <- sNCA(time,conc)
+    NCA_res <-sNCA(time,conc)
     aucall <- NCA_res[["AUCALL"]]
     aucinf <- NCA_res[["AUCIFP"]]
     cmax <- NCA_res[["CMAX"]]
@@ -24,11 +25,11 @@ performPlethemNCA <- function(result,var_names,mode = "FD"){
     tmax <- NCA_res[["TMAX"]]
     last_time <- time[length(time)]
     start_time <- ifelse(last_time < 24,0,last_time-24)
-    auc24 <- IntAUC(time,conc,start_time,last_time,NCA_Res)
+    auc24 <- IntAUC(time,conc,start_time,last_time,NCA_res)
     return(c("AUCall"=aucall,"AUCinf"=aucinf,"AUC24"=auc24,
              "Cmax"=cmax,"T_cmax"=tmax,
              "half_life"=hlfe,"slope"=termslope))
-  },conc_data, result$time)
+  s},conc_data, result$time)
   ncadf <- as.data.frame(nca_conc_list)
   colnames(ncadf)<- var_names
 
