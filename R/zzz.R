b3ee5bd4600952f2cdb089267e1e6cd53a093e9e
jfScitovation <jfitzpatrick@scitovation.com>
2020-06-25 11:57:05 -0400

    Change plethem starting message to reflect name change in function.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname,pkgname){
   packageStartupMessage("You are now using PLETHEM.
-                        If you have installed PLETHEM for the first time or have just updated it, please run setUserDb() to select an existing database or createUserDb() to create a new one.")
+                        If you have installed PLETHEM for the first time or have just updated it, please run setUserDb() to select an existing database or createEmptyUserDb() to create a new one.")
 }
